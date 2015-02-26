863f5db734a0a1dbfe571786c254d9b161812c1c
cclauss <cclauss@bluewin.ch>
2015-02-26 05:41:48 +0100

    In xrange(), zero is assumed if it is absent

--- snapchat_creep.py
+++ snapchat_creep.py
@@ -1,11 +1,11 @@
-from googlevoice import Voice
+import googlevoice
 import time
-from googlevoice.util import input
+#from googlevoice.util import input
 
-voice = Voice()
+voice = googlevoice.Voice()
 voice.login()
 
-for x in xrange(0,10):
-	for y in xrange(0,10):
+for x in xrange(10):
+	for y in xrange(10):
 		 voice.send_sms("5555512%s%s" % (`x`,`y`), "Add your message here") 
 		 time.sleep(10) #so Google won't rate limit
